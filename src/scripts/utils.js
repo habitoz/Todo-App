4b18b0023b8cb4d8b3c34e0a035d16b29f0fd213
haben mehar <habenmehari71@gmail.com>
2023-06-15 18:38:49 +0300

    linter problems resolved

--- src/scripts/utils.js
+++ src/scripts/utils.js
@@ -1,26 +1,25 @@
-
 const createDomList = (todo) => {
-   const listElement = document.createElement('li');
-   listElement.classList.add('list-item');
-   const todoDescriptionContainer= document.createElement('div');
-   const checkboxElement= document.createElement('input');
-   const todoDescription = document.createElement('p');
-   todoDescriptionContainer.classList.add('todo-description')
-   checkboxElement.setAttribute('type','checkbox');
-   checkboxElement.setAttribute('value',todo.completed);
-   todoDescription.innerHTML= todo.description;
-   todoDescriptionContainer.appendChild(checkboxElement);
-   todoDescriptionContainer.appendChild(todoDescription);
-   
-   const moreElement= document.createElement('i');
-   const deleteElement= document.createElement('i');
-   moreElement.classList.add('fa-solid', 'fa-ellipsis-vertical','icon', 'show');
-   deleteElement.classList.add('fa', 'fa-trash','icon', 'delete-icon', 'hide');
-   
-   listElement.appendChild(todoDescriptionContainer);
-   listElement.appendChild(deleteElement);
-   listElement.appendChild(moreElement)
-   return listElement;
-}
+  const listElement = document.createElement('li');
+  listElement.classList.add('list-item');
+  const todoDescriptionContainer = document.createElement('div');
+  const checkboxElement = document.createElement('input');
+  const todoDescription = document.createElement('p');
+  todoDescriptionContainer.classList.add('todo-description');
+  checkboxElement.setAttribute('type', 'checkbox');
+  checkboxElement.setAttribute('value', todo.completed);
+  todoDescription.innerHTML = todo.description;
+  todoDescriptionContainer.appendChild(checkboxElement);
+  todoDescriptionContainer.appendChild(todoDescription);
+
+  const moreElement = document.createElement('i');
+  const deleteElement = document.createElement('i');
+  moreElement.classList.add('fa-solid', 'fa-ellipsis-vertical', 'icon', 'show');
+  deleteElement.classList.add('fa', 'fa-trash', 'icon', 'delete-icon', 'hide');
+
+  listElement.appendChild(todoDescriptionContainer);
+  listElement.appendChild(deleteElement);
+  listElement.appendChild(moreElement);
+  return listElement;
+};
 
 export default createDomList;
